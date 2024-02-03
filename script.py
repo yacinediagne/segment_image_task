52282bef66b7c86433440342858a3137d38d7d55
yacinediagne <yacinediagne@esp.sn>
2024-02-03 20:34:50 +0100

    Scrypt modified to skip ve creation if exists

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
 import os
 import subprocess
 
-print("------------Step 1 : Virtual Environment creation -----------.")
-
+print("===============================Step 1 : Virtual Environment creation =================================")
 
 # Workdirectory path
 working_dir = os.getcwd()
@@ -10,23 +9,28 @@ working_dir = os.getcwd()
 # Environment Name
 venv_name = "venv"
 
-# Create virtual environment
-subprocess.run(["python", "-m", "venv", venv_name], check=True)
+# Path to the virtual environment
+venv_path = os.path.join(working_dir, venv_name)
 
-# Activate the environment
-if os.name == "posix":  # Linux
-    activate_path = os.path.join(working_dir, venv_name, "bin", "activate")
-    subprocess.run(["source", activate_path], shell=True, check=True)
-elif os.name == "nt":  # windowa
-    activate_path = os.path.join(working_dir, venv_name, "Scripts", "activate")
-    subprocess.run([activate_path], shell=True, check=True)
+# Check if the virtual environment exists
+if not os.path.exists(venv_path):
+    # Create virtual environment
+    subprocess.run(["python", "-m", "venv", venv_name], check=True)
+    
+    # Activate the environment
+    if os.name == "posix":  # Linux
+        activate_path = os.path.join(venv_path, "bin", "activate")
+        subprocess.run(["source", activate_path], shell=True, check=True)
+    elif os.name == "nt":  # Windows
+        activate_path = os.path.join(venv_path, "Scripts", "activate")
+        subprocess.run([activate_path], shell=True, check=True)
 
-# Requirements i
-subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
+    # Install requirements
+    subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
 
-print("Virtual environment created successfuly  ;).")
+print("=====================================Virtual environment created or exists already ;) ====================")
 
-print("------------Step 2 : Testing -----------.")
+print("===================================== Step 2 : Testing ==============================================")
 
-#Executing the Test
+# Run tests with pytest
 subprocess.run(["pytest", "test_microservice.py"], check=True)
